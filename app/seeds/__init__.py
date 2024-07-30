7498ce4ace85ad9d5641bbc7db4bf6d52a56b725
bradsimpson213 <bradsimpson@icloud.com>
2024-07-30 12:28:25 -0400

    fixed db seeding constraint issue

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -17,25 +17,25 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_transactions()
-        undo_starships()
-        undo_users()
+        # undo_transactions()
+        # undo_starships()
+        # undo_users()
         undo_shiptypes()
         undo_species()
     seed_species()
     seed_shiptypes()    
-    seed_users()
-    seed_starships()
-    seed_transactions()
+    # seed_users()
+    # seed_starships()
+    # seed_transactions()
     print("DB SEEDED")
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_transactions()
-    undo_starships()
-    undo_users()
+    # undo_transactions()
+    # undo_starships()
+    # undo_users()
     undo_shiptypes()
     undo_species()
     print("DB DESTROYED")
