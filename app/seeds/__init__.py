029a07e489c9d6ba625092af3bd497eb8efa9909
bradsimpson213 <bradsimpson@icloud.com>
2024-07-29 23:47:27 -0400

    updated seeder files

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -4,7 +4,7 @@ from .species import seed_species, undo_species
 from .shiptypes import seed_shiptypes, undo_shiptypes
 from .users import seed_users, undo_users
 from .starships import seed_starships, undo_starships
-
+from .transactions import seed_transactions, undo_transactions
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -17,6 +17,7 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_transactions()
         undo_starships()
         undo_users()
         undo_shiptypes()
@@ -25,12 +26,14 @@ def seed():
     seed_shiptypes()    
     seed_users()
     seed_starships()
+    seed_transactions()
     print("DB SEEDED")
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_transactions()
     undo_starships()
     undo_users()
     undo_shiptypes()
--- app/seeds/transactions.py
+++ app/seeds/transactions.py
@@ -38,24 +38,139 @@ def seed_transactions():
                         sale_date=datetime.datetime.now(),
         )
         
-        {'buyer': 27, 'seller': 28, 'starship': 5, 'sale_price': 100000000, 'sale_date': datetime.datetime.now()},
-        {'buyer': 22, 'seller': 24, 'starship': 6, 'sale_price': 249999, 'sale_date': datetime.datetime.now()},
-        {'buyer': 18, 'seller': 27, 'starship': 7, 'sale_price': 105000, 'sale_date': datetime.datetime.now()},
-        {'buyer': 21, 'seller': 12, 'starship': 8, 'sale_price': 157000, 'sale_date': datetime.datetime.now()},
-        {'buyer': 14, 'seller': 4, 'starship': 9, 'sale_price': 251000, 'sale_date': datetime.datetime.now()},
-        {'buyer': 4, 'seller': 21, 'starship': 10, 'sale_price': 150000, 'sale_date': datetime.datetime.now()},
-        {'buyer': 25, 'seller': 20, 'starship': 11, 'sale_price': 430000, 'sale_date': datetime.datetime.now()},
-        {'buyer': 24, 'seller': 22, 'starship': 12, 'sale_price': 148005, 'sale_date': datetime.datetime.now()},
-        {'buyer': 17, 'seller': 4, 'starship': 13, 'sale_price': 224725, 'sale_date': datetime.datetime.now()},
-        {'buyer': 5, 'seller': 27, 'starship': 14, 'sale_price': 425000, 'sale_date': datetime.datetime.now()},
-        {'buyer': 31, 'seller': 5, 'starship': 15, 'sale_price': 625000, 'sale_date': datetime.datetime.now()},
-        {'buyer': 9, 'seller': 28, 'starship': 16, 'sale_price': 215999, 'sale_date': datetime.datetime.now()},
-        {'buyer': 30, 'seller': 21, 'starship': 17, 'sale_price': 255999, 'sale_date': datetime.datetime.now()},
-        {'buyer': 33, 'seller': 32, 'starship': 18, 'sale_price': 182575, 'sale_date': datetime.datetime.now()},
-        {'buyer': 29, 'seller': 27, 'starship': 19, 'sale_price': 190500, 'sale_date': datetime.datetime.now()},
-        {'buyer': 28, 'seller': 11, 'starship': 20, 'sale_price': 65000, 'sale_date': datetime.datetime.now()}
+        transaction_5 = Transaction(
+                        buyer=27, 
+                        seller=28, 
+                        starship=5, 
+                        sale_price=100000000, 
+                        sale_date=datetime.datetime.now(),
+        )
+
+        transaction_6 = Transaction(
+                        buyer=22, 
+                        seller=24, 
+                        starship=6, 
+                        sale_price=249999, 
+                        sale_date=datetime.datetime.now(),
+        )
         
-        all_transactions = [ transaction_1, transaction_2, transaction_3, transaction_4,  ]
+        transaction_7 = Transaction(
+                        buyer=18, 
+                        seller=27, 
+                        starship=7, 
+                        sale_price=105000, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_8 = Transaction(
+                        buyer=21, 
+                        seller=12, 
+                        starship=8, 
+                        sale_price=157000, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_9 = Transaction(
+                        buyer=14, 
+                        seller=4, 
+                        starship=9, 
+                        sale_price=251000, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_10 = Transaction(
+                        buyer=4, 
+                        seller=21, 
+                        starship=10, 
+                        sale_price=150000, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_11 = Transaction(
+                        buyer=25, 
+                        seller=20, 
+                        starship=11, 
+                        sale_price=430000, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_12 = Transaction(
+                        buyer=24, 
+                        seller=22, 
+                        starship=12, 
+                        sale_price=148005, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_13 = Transaction(
+                        buyer=17, 
+                        seller=4, 
+                        starship=13, 
+                        sale_price=224725, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_14 = Transaction(
+                        buyer=5, 
+                        seller=27, 
+                        starship=14, 
+                        sale_price=425000, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_15 = Transaction(
+                        buyer=31, 
+                        seller=5, 
+                        starship=15, 
+                        sale_price=625000, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_16 = Transaction(
+                        buyer=9, 
+                        seller=28, 
+                        starship=16, 
+                        sale_price=215999, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_17 = Transaction(
+                        buyer=30, 
+                        seller=21, 
+                        starship=17, 
+                        sale_price=255999, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_18 = Transaction(
+                        buyer=33, 
+                        seller=32, 
+                        starship=18, 
+                        sale_price=182575, 
+                        sale_date=datetime.datetime.now(),
+        )
+
+        transaction_19 = Transaction(
+                        buyer=29, 
+                        seller=27, 
+                        starship=19, 
+                        sale_price=190500, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        transaction_20 = Transaction(
+                        buyer=28, 
+                        seller=11, 
+                        starship=20, 
+                        sale_price=65000, 
+                        sale_date=datetime.datetime.now(),
+        )
+        
+        all_transactions = [ transaction_1, transaction_2, transaction_3, transaction_4, transaction_5,
+                            transaction_6, transaction_7, transaction_8, transaction_9, transaction_10,
+                            transaction_11, transaction_12, transaction_13, transaction_14, transaction_15,
+                            transaction_16, transaction_17, transaction_18, transaction_19, transaction_20 ]
+        
         _ = [db.session.add(transaction) for transaction in all_transactions]
         db.session.commit()
 
@@ -64,7 +179,7 @@ def seed_transactions():
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
-def undo_starships():
+def undo_transactions():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.transactions RESTART IDENTITY CASCADE;")
     else:
