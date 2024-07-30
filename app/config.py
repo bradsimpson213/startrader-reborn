991923d385c6e39c929657780ab0ced22879b70f
bradsimpson213 <bradsimpson@icloud.com>
2024-07-30 10:01:47 -0400

    updated seeder files

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-key-for-devs'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
